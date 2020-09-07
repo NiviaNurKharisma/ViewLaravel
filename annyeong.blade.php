0ad2dc5ff262db62b0622975e24f44cca35b490f
Nivia Nur Kharisma <53246336+NiviaNurKharisma@users.noreply.github.com>
2020-09-07 11:09:53 +0700

    Add files via upload

---
+++ annyeong.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel</title>
+    <style>
+        html, body {
+                background-color: #fff;
+                color: #1e90ff;
+                font-family: 'Arial', sans-serif;
+                font-weight: 300;
+                text-align: center;
+        }
+        h1{
+            margin-top: 0px;margin-bottom: 10px;
+            color:#1e90ff;
+            font-size: 230%;
+            font-weight: 300;
+            text-transform: uppercase;
+            word-spacing: 4px;
+            letter-spacing: 1px; 
+        }
+        
+        .content{
+            width: 100vh;
+            height: 200px;
+            position: absolute;
+            margin: auto;
+            left: 0;
+            right: 0;
+            top: 0;
+            bottom: 0;
+        }
+        .box{
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <div class= "content">
+        <div class= "box">
+            <p> Dari annyeong <p>
+            <h1>Nivia Nur Kharisma - XII RPL 1</h1>
+             <h4>annyeonghaseyo</h4> 
+        </div>
+    </div>
+   
+</body>
+</html>
